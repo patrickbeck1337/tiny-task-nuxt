9aa13deb0120a75585dcbe901f629fa35b70be27
Patrick Beck <patrick.beck@coyoapp.com>
2020-02-11 17:08:16 +0100

    Add form submit

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,23 @@
     max-width="600"
     class="mx-auto"
   >
-    <v-card-text>
-      <v-text-field
-        label="Add new tiny task..."
-      ></v-text-field>
-    </v-card-text>
+    <v-form
+      @submit="submitForm"
+      onSubmit="return false;"
+    >
+      <v-card-text>
+        <v-text-field
+          v-model="newTask"
+          label="Add new tiny task..."
+          required
+        ></v-text-field>
+      </v-card-text>
+    </v-form>
     <v-divider class="mx-4"></v-divider>
     <v-list two-line subheader>
       <v-list-item
-        v-for="item in items"
-        :key="item.title"
+        v-for="task in tasks"
+        :key="task.id"
         @click=""
       >
         <v-list-item-avatar>
@@ -22,7 +29,8 @@
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title v-text="item.name"></v-list-item-title>
+          <v-list-item-title v-text="task.name"></v-list-item-title>
+          <v-list-item-subtitle v-text="task.id"></v-list-item-subtitle>
         </v-list-item-content>
         <v-btn text icon>
           <v-icon>delete</v-icon>
@@ -33,13 +41,21 @@
 </template>
 
 <script>
+  import uuid from 'uuid';
+
   export default {
     data() {
       return {
-        items: [{id: '123', name: 'Buy a house'}, {id: '123', name: 'Buy a book'}],
+        newTask: '',
+        tasks: [{id: uuid(), name: 'Buy a house'}, {id: uuid(), name: 'Buy a book'}],
         icon: 'assignment',
         iconClass: 'blue white--text'
       };
+    },
+    methods: {
+      submitForm() {
+        this.tasks.push({id: uuid(), name: this.newTask});
+      }
     }
   };
 </script>
